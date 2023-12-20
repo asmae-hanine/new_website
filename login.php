d12ab446c7f6a59283459beefe7de7618adcfffe
Asmae <asmaelhama078@gmail.com>
2023-12-20 07:58:13 +0100

    modify login.php

--- login.php
+++ login.php
@@ -7,13 +7,15 @@
 </head>
 
 <body>
-    <label for="email">Email</label>
-    <input type="email" name="email" id="email">
+    <form action="connect.php" method="post">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email">
 
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
 
-    <input type="submit" name="submit" id="submit" value="Log In">
+        <input type="submit" name="submit" id="submit" value="Log In">
+    </form>
 </body>
 
 </html>
